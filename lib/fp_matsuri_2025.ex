7fa2a836f18577028bd475e0ccfcff6dc0a15a3b
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-06-12 15:38:00 +0900

    Initial commit

---
+++ lib/fp_matsuri_2025.ex
@@ -0,0 +1,18 @@
+defmodule FpMatsuri2025 do
+  @moduledoc """
+  Documentation for `FpMatsuri2025`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FpMatsuri2025.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fp_matsuri_2025_test.exs
@@ -0,0 +1,8 @@
+defmodule FpMatsuri2025Test do
+  use ExUnit.Case
+  doctest FpMatsuri2025
+
+  test "greets the world" do
+    assert FpMatsuri2025.hello() == :world
+  end
+end
